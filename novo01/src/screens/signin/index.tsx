959f80a20a476bdd5b2dda531e7f9c9f74380ad5
ribeiroian88 <ribeiroian870@gmail.com>
2025-09-12 07:50:04 -0300

    sfdkofsdofos

---
+++ novo01/src/screens/signin/index.tsx
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+export function SignIn(){
+    return(
+     <view style={{flex:1,justifyContent: "center", alignItems: "center"}}>
+        <Text>Essa é a tela de SignIn</Text>
+     </view>
+    )
+}
